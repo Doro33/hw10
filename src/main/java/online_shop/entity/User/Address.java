f05bfcbfa3bd2fadc4e94cc7f802f4e5ebaef1b3
doro33 <sin.mhm@gmail.com>
2022-08-24 14:58:37 +0430

    add Address class.

---
+++ src/main/java/online_shop/entity/User/Address.java
@@ -0,0 +1,40 @@
+package online_shop.entity.User;
+
+public class Address {
+    String city;
+    String street;
+    String alley;
+    int houseNumber;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAlley() {
+        return alley;
+    }
+
+    public void setAlley(String alley) {
+        this.alley = alley;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
