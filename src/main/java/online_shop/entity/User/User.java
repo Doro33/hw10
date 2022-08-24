79adef76facbf27c234c7170ad767b23ad5046a9
doro33 <sin.mhm@gmail.com>
2022-08-24 16:37:12 +0430

    add User class

---
+++ src/main/java/online_shop/entity/User/User.java
@@ -0,0 +1,61 @@
+package online_shop.entity.User;
+
+import online_shop.entity.order.Order;
+
+import java.util.ArrayList;
+
+public class User {
+private String name;
+private String nationalCode;
+private String Username;
+private String Password;
+private ArrayList<Address> addresses;
+private ArrayList<Order> orders;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+    public ArrayList<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(ArrayList<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+}
