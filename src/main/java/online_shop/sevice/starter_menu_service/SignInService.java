61ca9d61338db91f667e9cc9d30c8d427b03613d
doro33 <sin.mhm@gmail.com>
2022-08-29 17:26:57 +0430

    add signInService class

---
+++ src/main/java/online_shop/sevice/starter_menu_service/SignInService.java
@@ -0,0 +1,24 @@
+package online_shop.sevice.starter_menu_service;
+
+import online_shop.entity.client.Client;
+import online_shop.util.AppContext;
+
+import java.sql.SQLException;
+
+public class SignInService {
+    public static boolean usernameExist(String username) {
+        try {
+            return AppContext.getClientRepository().usernameExist(username);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Client findClientByUsername(String username){
+        try {
+            return AppContext.getClientRepository().findByUsername(username);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
