614514ab381ff66572ab4121751eca18263b7809
doro33 <sin.mhm@gmail.com>
2022-08-29 13:34:45 +0430

    add StarterMenu class

---
+++ src/main/java/online_shop/view/StarterMenu.java
@@ -0,0 +1,37 @@
+package online_shop.view;
+
+import online_shop.util.AppContext;
+
+import java.util.ArrayList;
+
+public class StarterMenu {
+    ArrayList<String> menuItems= new ArrayList<>();
+    {
+        menuItems.add("sign in");
+        menuItems.add("sign up");
+        menuItems.add("exit");
+    }
+    public void startMenu(){
+        AppContext.printStrings(menuItems);
+        System.out.print("what do you want to do: ");
+        String input = AppContext.getScanner().nextLine();
+        firsLoop:
+        while (!input.isEmpty()) {
+            switch (input) {
+                case "1":
+                    System.out.println("----------------");
+                    break firsLoop;
+                case "2":
+                    System.out.println("----------------");
+                    break firsLoop;
+                case "3":
+                    System.out.println("----------------");
+                    break firsLoop;
+                default:
+                    System.out.println("your input is not valid.");
+                    System.out.print("you can only choose 1-3 : ");
+                    input=AppContext.getScanner().nextLine();
+            }
+        }
+    }
+}
